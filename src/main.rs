713d5745bdf8e7f52595dafde7e4e28a1a264ef7
xero-37 <anaghatri164268a@gmail.com>
2026-05-30 23:42:35 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -20,4 +20,10 @@ fn main() {
     // The following call will panic until implemented in lib.rs
     let amount_out = v2_math::calculate_swap_output(r_x, r_y, amount_in, fee);
     println!("Swap Result: {} Y", amount_out);
+    let l_depth=1000000.0;
+    let current_price=1.0;
+    let p_a=0.99;
+    let p_b=1.01;
+    let (x,y) = calculate_real_reserves(l_depth,current_price,p_a,p_b);
+    println!("The x_real is {} and y_real is {}",x,y);
 }
